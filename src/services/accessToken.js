158f569bf1ecf6d6d05a4db6b64ab3ff7a337838
rodrigo <rodrigojaviergd@gmail.com>
2023-09-26 13:36:17 -0300

    stable version

--- src/services/accessToken.js
+++ src/services/accessToken.js
@@ -3,7 +3,8 @@ const { pool_pg } = require('../database');
 
 module.exports.returnAccessTokenForItemId = async (urlAcTok) => {
   try {
-    const accessToken = await fetch.post(`https://api.mercadolibre.com/oauth/token?grant_type=refresh_token&client_id=3698739416306050&client_secret=LwgOJm33WigcfXp8AL2OyKZN6FS4YoME&refresh_token=${urlAcTok.trim()}&limit=200`, {
+    const accessToken = await fetch
+      .post(`https://api.mercadolibre.com/oauth/token?grant_type=refresh_token&client_id=3698739416306050&client_secret=LwgOJm33WigcfXp8AL2OyKZN6FS4YoME&refresh_token=${urlAcTok.trim()}&limit=200`, {
       headers: { "Content-Type": "application/json" }
     })
       .then(data => data).catch(err => console.log(`Error en el catch del return access toekn`, err['response']['data']['message']));
