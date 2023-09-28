19bb83f478366b71b23f5223cb33181cfe754a40
rodrigo <rodrigojaviergd@gmail.com>
2023-09-28 18:15:44 -0300

    back to server

--- src/routes/excel.routes.js
+++ src/routes/excel.routes.js
@@ -2,7 +2,6 @@ const { Router } = require('express')
 const router = Router();
 const ctrl = require('../controllers/excel.controller');
 
-
 router.post('/uploadInvoiceDoc', async (req, res) => { await ctrl.uploadInvoiceDoc(req, res); });
 
 module.exports = router;
