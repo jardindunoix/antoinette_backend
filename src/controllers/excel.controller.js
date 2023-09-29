ff9c24312528d7ccd65cbf18fc74d5167f2caba5
rodrigo <rodrigojaviergd@gmail.com>
2023-09-28 21:02:33 -0300

    back to server

--- src/controllers/excel.controller.js
+++ src/controllers/excel.controller.js
@@ -24,19 +24,19 @@ const uploadInvoiceDoc = async (req, res) => {
     }
 
     // BODY OF EXCEL
-    if (Object.entries(el).length > 9 && index < 50) {
+    if (Object.entries(elem).length > 9 && index < 50) {
       invoiceRows.push({
-        costumer_code: Object.entries(el)[0][1], // Invoice: 1338893,  
-        description: Object.entries(el)[1][1], // __EMPTY_1: 'watch',  
-        sku: Object.entries(el)[2][1], // __EMPTY_2: 'NNTQ79237',  
-        quantity: Object.entries(el)[3][1], // __EMPTY_3: 20, 
-        materials: Object.entries(el)[4][1], // __EMPTY_4: 'Metal + plastic',  
-        hs_code: Object.entries(el)[5][1], // __EMPTY_5: 9102120000,  
-        selling_value: Object.entries(el)[6][1], // __EMPTY_6: 82.348,   
-        unit_value: Object.entries(el)[7][1], // __EMPTY_7: 69.2,  
-        divi: Object.entries(el)[8][1], // __EMPTY_8: 'USD', 
-        total_value: Object.entries(el)[9][1], // __EMPTY_9: 1384,  
-        mlc: Object.entries(el)[10][1], // __EMPTY_10: 'MLC546055202'  
+        costumer_code: Object.entries(elem)[0][1], // Invoice: 1338893,  
+        description: Object.entries(elem)[1][1], // __EMPTY_1: 'watch',  
+        sku: Object.entries(elem)[2][1], // __EMPTY_2: 'NNTQ79237',  
+        quantity: Object.entries(elem)[3][1], // __EMPTY_3: 20, 
+        materials: Object.entries(elem)[4][1], // __EMPTY_4: 'Metal + plastic',  
+        hs_code: Object.entries(elem)[5][1], // __EMPTY_5: 9102120000,  
+        selling_value: Object.entries(elem)[6][1], // __EMPTY_6: 82.348,   
+        unit_value: Object.entries(elem)[7][1], // __EMPTY_7: 69.2,  
+        divi: Object.entries(elem)[8][1], // __EMPTY_8: 'USD', 
+        total_value: Object.entries(elem)[9][1], // __EMPTY_9: 1384,  
+        mlc: Object.entries(elem)[10][1], // __EMPTY_10: 'MLC546055202'  
       })
     }
   });
