2988bfb5a2ff9f5200cdf6f3f3b738b390454b5d
rodrigo <rodrigojaviergd@gmail.com>
2023-09-29 22:06:27 -0300

    back to server

--- src/services/excelService/excel.js
+++ src/services/excelService/excel.js
@@ -2,12 +2,24 @@ const { pool_pg } = require('../../database');
 
 async function insertExcelData(requestList) {
   try {
+    let values = ''
+    const coma = ', '
+    requestList.forEach((el, index) => {
+      console.log(index + 1)
+      values += `('${el.invoice_number}', '${el.invoice_date}', '${el.invoice_terms}', '${el.costumer_code}', '${el.description}', '${el.sku}', '${el.quantity}', '${el.materials}', '${el.hs_code}', '${el.selling_value}', '${el.unit_value}', '${el.divisa}', '${el.total_value}', '${el.mlc}')`
+      values += index < listLast.length - 1 ? coma + "\n" : ''
+    })
+
+    const queryInsert = `INSERT INTO invoice (invoice_number, invoice_date, invoice_terms, costumer_code, description, sku, quantity, materials, hs_code, selling_value, unit_value, divisa, total_value, mlc) VALUES
+  ${values};`;
+
     await pool_pg.query(queryInsert);
     return 'ok'
   } catch (error) {
-    console.log('error ionserting in invoice table', error)
-    return 'error inserting in invoice table'
+    console.log('error inserting', error)
+    return 'error inserting'
   }
+
 }
 
 module.exports = {
