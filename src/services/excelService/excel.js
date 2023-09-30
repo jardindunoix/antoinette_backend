02a8ac64d61709eb371c9e6a5855ac4053d001d2
rodrigo <rodrigojaviergd@gmail.com>
2023-09-29 22:32:47 -0300

    back to server

--- src/services/excelService/excel.js
+++ src/services/excelService/excel.js
@@ -5,17 +5,15 @@ async function insertExcelData(requestList) {
     let values = ''
     const coma = ', '
     const inventoryColumn = requestList[0].invoice_number
-
-    console.log(inventoryColumn)
-
     requestList.forEach((el, index) => {
       values += `('${el.invoice_number}', '${el.invoice_date}', '${el.invoice_terms}', '${el.costumer_code}', '${el.description}', '${el.sku}', '${el.quantity}', '${el.materials}', '${el.hs_code}', '${el.selling_value}', '${el.unit_value}', '${el.divisa}', '${el.total_value}', '${el.mlc}')`
       values += index < requestList.length - 1 ? coma + "\n" : ''
     })
-
+    const queryDelete = `DELELTE FROM invoice WHERE invoice_number = ${inventoryColumn};`
     const queryInsert = `INSERT INTO invoice (invoice_number, invoice_date, invoice_terms, costumer_code, description, sku, quantity, materials, hs_code, selling_value, unit_value, divisa, total_value, mlc) VALUES
   ${values};`;
 
+    await pool_pg.query(queryDelete);
     await pool_pg.query(queryInsert);
     return 'ok'
   } catch (error) {
