63d148f56112f58639b3c51d2e9d4e8c6069eae0
rodrigo <rodrigojaviergd@gmail.com>
2023-09-29 22:13:26 -0300

    back to server

--- src/services/excelService/excel.js
+++ src/services/excelService/excel.js
@@ -8,22 +8,21 @@ async function insertExcelData(requestList) {
     console.table(requestList)
     console.log(requestList.length)
 
-  //   requestList.forEach((el, index) => {
-  //     console.log(index + 1)
-  //     values += `('${el.invoice_number}', '${el.invoice_date}', '${el.invoice_terms}', '${el.costumer_code}', '${el.description}', '${el.sku}', '${el.quantity}', '${el.materials}', '${el.hs_code}', '${el.selling_value}', '${el.unit_value}', '${el.divisa}', '${el.total_value}', '${el.mlc}')`
-  //     values += index < requestList.length - 1 ? coma + "\n" : ''
-  //   })
+    requestList.forEach((el, index) => {
+      console.log(index + 1)
+      values += `('${el.invoice_number}', '${el.invoice_date}', '${el.invoice_terms}', '${el.costumer_code}', '${el.description}', '${el.sku}', '${el.quantity}', '${el.materials}', '${el.hs_code}', '${el.selling_value}', '${el.unit_value}', '${el.divisa}', '${el.total_value}', '${el.mlc}')`
+      values += index < requestList.length - 1 ? coma + "\n" : ''
+    })
 
-  //   const queryInsert = `INSERT INTO invoice (invoice_number, invoice_date, invoice_terms, costumer_code, description, sku, quantity, materials, hs_code, selling_value, unit_value, divisa, total_value, mlc) VALUES
-  // ${values};`;
+    const queryInsert = `INSERT INTO invoice (invoice_number, invoice_date, invoice_terms, costumer_code, description, sku, quantity, materials, hs_code, selling_value, unit_value, divisa, total_value, mlc) VALUES
+  ${values};`;
 
-  //   await pool_pg.query(queryInsert);
+    await pool_pg.query(queryInsert);
     return 'ok'
   } catch (error) {
     console.log('error inserting', error)
     return 'error inserting'
   }
-
 }
 
 module.exports = {
