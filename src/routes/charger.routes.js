b13611847091cdaec5453ab68018bbb946403f6e
rodrigo <rodrigojaviergd@gmail.com>
2023-10-31 18:09:42 -0300

    back to server

--- src/routes/charger.routes.js
+++ src/routes/charger.routes.js
@@ -2,6 +2,6 @@ const { Router } = require('express')
 const router = Router();
 const ctrl = require('../controllers/charger.controller');
 
-router.get('/:email', async (req, res) => { await ctrl.emailExists(req, res); });
+router.get('auth/exists/:company/:email', async (req, res) => { await ctrl.emailExists(req, res); });
 
 module.exports = router;
