7b8a25252de586bd5c13ed69cb4e7d3c5dc2092f
rodrigo <rodrigojaviergd@gmail.com>
2023-10-31 19:02:35 -0300

    back to server

--- src/controllers/charger.controller.js
+++ src/controllers/charger.controller.js
@@ -1,21 +1,30 @@
 
+const { result_fake } = require('../services/charger/pools')
+
 /* call excel */
 const emailExists = async (req, res) => {
   try {
-    console.log("?>??>?>?>?>?>?>")
+
+    const { email } = req.params
     console.log(req.params)
+    const email_ = email.toLowerCase()
+    console.log(`${email_ === "rgarrido@dhemax.com"} - ${email_} --`)
+    const statusValue = email_ === "rgarrido@dhemax.com" ? 200 : 400
 
-    // const { email } = req.params
-    // console.log(req.params)
-    // const email_ = email.toLowerCase()
-    // console.log(`${email_ === "rgarrido@dhemax.com"} - ${email_} --`)
-    // const statusValue = email_ === "rgarrido@dhemax.com" ? 200 : 400
-
-    // // res.status(200).json({ "resp": (email_ === "rgarrido@dhemax.com") })
-    // res.status(statusValue).json({ "resp": (email_ === "rgarrido@dhemax.com") })
+    // res.status(200).json({ "resp": (email_ === "rgarrido@dhemax.com") })
+    res.status(statusValue).json({ "resp": (email_ === "rgarrido@dhemax.com") })
 
 
-    res.status(200).json({ "resp": true })
+    // res.status(200).json({ "resp": true })
+  } catch (error) {
+    console.log('error', error)
+  }
+}
+
+const getPools = (req, res) => {
+  try {
+    console.log(" -- POOLS -- ")
+    res.status(200).json(result_fake)
   } catch (error) {
     console.log('error', error)
   }
