39e4e0f86c73284357c72ce145080406bf14a9f9
rodrigo <rodrigojaviergd@gmail.com>
2023-11-22 13:54:51 -0300

    back to server

--- src/controllers/charger.controller.js
+++ src/controllers/charger.controller.js
@@ -7,7 +7,7 @@ const emailExists = async (req, res) => {
     const { email } = req.params
     // console.log(req.params)
     const email_ = email.toLowerCase()
-    // console.log(`${email_ === "rgarrido@dhemax.com"} - ${email_} --`)
+    console.log(`${email_ === "rgarrido@dhemax.com"} - ${email_} --`)
     const statusValue = email_ === "rgarrido@dhemax.com" ? 200 : 400
     // res.status(200).json({ "resp": (email_ === "rgarrido@dhemax.com") })
     res.status(statusValue).json({ "resp": (email_ === "rgarrido@dhemax.com") })
